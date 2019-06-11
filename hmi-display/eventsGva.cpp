b63182eacc570b503e18f1566a68b123ccaf4311
Ross Newman <ross@rossnewman.com>
2019-06-11 07:45:37 +0100

    Adding new event class

---
+++ hmi-display/eventsGva.cpp
@@ -0,0 +1,148 @@
+#include <cairo-xlib.h>
+#include "gva.h"
+#include "eventsGva.h"
+
+eventsGva::eventsGva(Display *display, Window *window) { 
+  m_display = display;
+  m_window = window;
+  
+  /* 
+   * select kind of events we are interested in 
+   */
+  XSelectInput (m_display, *m_window,
+                KeyPressMask | KeyReleaseMask | 
+                ButtonPressMask	| ButtonReleaseMask |
+                ExposureMask  | StructureNotifyMask);
+}
+
+int 
+eventsGva::nextGvaEvent(eventGvaType *event) {
+  XEvent e;
+  XLockDisplay (m_display);
+  XNextEvent (m_display, &e);
+  
+  event->type = NO_EVENT;
+
+  switch (e.type) {
+    case KeyPress:
+      {
+        event->type = KEY_EVENT;
+        switch (e.xkey.keycode)
+          {
+          case 0x09:
+            /* exit on ESC key press */
+            event->key = KEY_ESC;
+            break;
+          case 0xa:
+            /* 1 maps to SA */
+            event->key = KEY_SA;
+            break;
+          case 0xb:
+            /* 2 maps to WPN */
+            event->key = KEY_WPN;
+            break;
+          case 0xc:
+            /* 3 maps to DEF */
+            event->key = KEY_DEF;
+            break;
+          case 0xd:
+            /* 4 maps to SYS */
+            event->key = KEY_SYS;
+            break;
+          case 0xe:
+            /* 5 maps to DRV */
+            event->key = KEY_DRV;
+            break;
+          case 0xf:
+            /* 6 maps to STR */
+            event->key = KEY_STR;
+            break;
+          case 0x10:
+            /* 7 maps to COM */
+            event->key = KEY_COM;
+            break;
+          case 0x11:
+            /* 8 maps to BMS */
+            event->key = KEY_BMS;
+            break;
+          case 0x29:
+            /* f toggle fullscreen TODO: Does not work */
+            event->key = KEY_FULLSCREEN;
+            break;
+          case 0x2d:
+            /* k toggle keyboard */
+            event->key = KEY_KEYBOARD;
+            break;
+          case 0x42:
+            /* caps_lock keyboard */
+            event->key = KEY_F17;
+            break;
+          case 0x4D:
+            /* num_lock keyboard */
+            event->key = KEY_F18;
+            break;
+          case 0x2e:
+            /* l show / hide labels */
+            event->key = KEY_F19;
+            break;
+          default:
+            printf ("KeyPress not defined 0x%x\n", e.xkey.keycode);
+            break;
+          }
+      }      
+    case KeyRelease:
+      {
+        /* Nothing here yet */
+      }
+      break;
+    case 25:
+        printf("Resize %d\n", 25);
+    case ConfigureNotify:
+      {
+        XConfigureEvent *cev = &e.xconfigure;
+        event->type = RESIZE_EVENT;
+        event->resize.width = cev->width;
+        event->resize.height = cev->height;
+        printf("Resize %d %d %d\n", ConfigureNotify,  cev->width,  cev->height);
+      }
+      break;
+    case Expose:
+      {
+        event->type = REDRAW_EVENT;
+      }
+      break;
+    case ButtonPressMask :
+    case ButtonReleaseMask :
+    break;
+    default:
+      {
+        /* Unknown X11 key event */
+        printf("Unknown X11 event %d\n",e.type);
+      }
+      break;
+  }
+  XUnlockDisplay (m_display);
+
+#if 0
+  while ( nextGvaEvent(event) == GVA_SUCCESS ) {
+    /* Process other events */
+  }
+#endif
+  return GVA_SUCCESS;
+}
+
+void
+eventsGva::flush() {
+  if (m_display) {
+    XClientMessageEvent dummyEvent;
+
+    XSync (m_display, true);
+
+    dummyEvent.type = Expose;
+    dummyEvent.window = *m_window;
+    dummyEvent.format = 32;
+
+    XSendEvent (m_display, *m_window, False, StructureNotifyMask, (XEvent *) & dummyEvent);
+    XFlush (m_display);
+  }
+}
---
+++ hmi-display/eventsGva.h
@@ -0,0 +1,89 @@
+#ifndef EVENTS_GVA_H
+#define EVENTS_GVA_H
+#include <X11/Xlib.h>
+#include "gva.h"
+
+using namespace gva;
+
+enum gvaKeyEnum 
+{
+  /* These are physical GVA keys around the display */
+  KEY_SA = 0,
+  KEY_WPN,
+  KEY_DEF,
+  KEY_SYS,
+  KEY_DRV,
+  KEY_STR,
+  KEY_COM,
+  KEY_BMS,
+  KEY_F1,
+  KEY_F2,
+  KEY_F3,
+  KEY_F4,
+  KEY_F5,
+  KEY_F6,
+  KEY_F7,
+  KEY_F8,
+  KEY_F9,
+  KEY_F10,
+  KEY_F11,
+  KEY_F12,
+  KEY_F13,
+  KEY_F14,
+  KEY_F15,
+  KEY_F16,
+  KEY_F17,
+  KEY_F18,
+  KEY_F19,
+  KEY_F20,
+  KEY_F21,
+  KEY_F22,
+  KEY_BLACKOUT,
+  KEY_POWER,
+  KEY_BRIGHTNESS_UP,
+  KEY_BRIGHTNESS_DOWN,
+  /* These are fake keys that only exist in the software (emulated world) */
+  KEY_ESC,
+  KEY_FULLSCREEN,
+  KEY_KEYBOARD
+};
+
+enum eventEnumType {
+  NO_EVENT = 0,
+  KEY_EVENT,
+  TOUCH_EVENT,
+  DDS_EVENT,
+  RESIZE_EVENT,
+  REDRAW_EVENT
+};
+
+struct touchType {
+  int x;
+  int y;
+};
+
+struct resolutionType {
+  int width;
+  int height;
+};
+
+class eventGvaType {
+public:
+  eventEnumType type;
+  gvaKeyEnum key;
+  touchType toutch;
+  resolutionType resize;
+};
+
+class eventsGva {
+public:
+  eventsGva() { m_display = 0; };
+  eventsGva(Display *display, Window *window); 
+  int nextGvaEvent(eventGvaType *event); // Use for X11/DDS/Touch events
+  void flush();
+private:
+ Display *m_display = 0;
+ Window *m_window = 0;
+};
+
+#endif
